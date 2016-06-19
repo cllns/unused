b75ce8a68c4deec0fee55709be36cfd6f0d030a1
Joshua Clayton <joshua.clayton@gmail.com>
2016-06-19 07:24:01 -0400

    newtype for git output

--- src/Unused/GitContext.hs
+++ src/Unused/GitContext.hs
@@ -9,6 +9,8 @@ import qualified Data.List as L
 import System.Process
 import Unused.Types (TermResults(trGitContext), GitContext(..), GitCommit(..), RemovalLikelihood(High), removalLikelihood, resultAliases)
 
+newtype GitOutput = GitOutput { unOutput :: String }
+
 gitContextForResults :: Int -> (String, TermResults) -> IO [(String, TermResults)]
 gitContextForResults commitCount a@(token, results) =
     case removalLikelihood results of
@@ -21,13 +23,13 @@ gitContextForResults commitCount a@(token, results) =
 -- 307dd20 Introduce internal yaml configuration of auto low likelihood match handling
 -- 3b627ee Allow multiple matches with single-occurring appropriate tokens
 -- f7a2e1a Add Hspec and tests around parsing
-logToGitContext :: String -> GitContext
+logToGitContext :: GitOutput -> GitContext
 logToGitContext =
-    GitContext . map GitCommit . shaList
+    GitContext . map GitCommit . shaList . unOutput
   where
     shaList = map (T.unpack . head . T.splitOn " " . T.pack) . lines
 
-gitLogSearchFor :: Int -> [String] -> IO String
+gitLogSearchFor :: Int -> [String] -> IO GitOutput
 gitLogSearchFor commitCount ts = do
   (_, results, _) <- readProcessWithExitCode "git" ["log", "-G", L.intercalate "|" ts, "--oneline", "-n", show commitCount] ""
-  return results
+  return $ GitOutput results
